57cb3abc69734a6dd3f865eccb5074af25323703
Joseph Freeston <snorklerjoe@gmail.com>
2023-04-11 19:13:22 -0400

    lib packaging

---
+++ package.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Packages this beacon code into a cute little zip file :)
+
+./tools/clean.sh
+
+wget https://raw.githubusercontent.com/adafruit/Adafruit_CircuitPython_IRRemote/main/adafruit_irremote.py -O ./lib/adafruit_irremote.py
+
+zip -r beacon-code.zip . -x .gitignore -x .gitmodules -x .git -x servercom -x tools/\* -x .git/\* -x lib/.git/\* -x servercom/\* -x package.sh # 2>&1 > /dev/null
+echo beacon-code.zip
--- tools/clean.sh
+++ tools/clean.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # A utility for cleaning things up before releases and such
 
-git submodule update --init --recursive
+rm ./lib/adafruit_irremote.py || echo no irremote installed so none to remove.
+rm ./beacon-code.zip || echo no zip generated so none to remove
 
+git submodule update --init --recursive
