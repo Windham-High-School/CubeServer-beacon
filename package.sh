eed84ba1bfed66a53e695a96211c1f5e85d697cf
Joseph Freeston <snorklerjoe@gmail.com>
2023-04-30 21:00:50 -0400

    Add submodule updating

--- package.sh
+++ package.sh
@@ -5,5 +5,7 @@
 
 wget https://raw.githubusercontent.com/adafruit/Adafruit_CircuitPython_IRRemote/main/adafruit_irremote.py -O ./lib/adafruit_irremote.py
 
+git submodule update --remote
+
 zip -r beacon-code.zip . -x .gitignore -x .gitmodules -x .git -x servercom -x tools/\* -x .git/\* -x lib/.git/\* -x servercom/\* -x package.sh # 2>&1 > /dev/null
 echo beacon-code.zip
